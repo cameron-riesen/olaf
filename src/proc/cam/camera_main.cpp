2a2f3e7445f596faa4b4593baac7f24f01ea0f4c
Cameron Riesen <cameron.riesen@gmail.com>
2016-10-10 03:43:08 +0000

    Added a test message between camera and GPS. Leaving it in as an example of pub/sub for the other processes.

--- src/proc/cam/camera_main.cpp
+++ src/proc/cam/camera_main.cpp
@@ -3,7 +3,9 @@
 #include <log4cplus/configurator.h>
 #include <log4cplus/initializer.h>
 
-#include <dds/dds.hpp>
+#include "cam_config_msg_DCPS.hpp"
+
+#include <iostream>
 
 int main()
 {
@@ -17,4 +19,22 @@ int main()
   LOG4CPLUS_INFO(console_logger, "Camera main started");
 
   dds::domain::DomainParticipant participant(0);
+  dds::topic::Topic<Cam_Messages::Config_Message> topic(participant, "cam_messages_config_message");
+  dds::pub::Publisher publisher(participant);
+  dds::pub::DataWriter<Cam_Messages::Config_Message> data_writer(publisher, topic);
+
+  Cam_Messages::Config_Message config_message(false, 10, 10, 1000);
+
+  auto matches = data_writer.publication_matched_status();
+
+  std::cout << "Waiting..." << "\n";
+  
+  while (matches.current_count())
+  {
+    matches = data_writer.publication_matched_status();
+  }
+
+  data_writer.write(config_message);
+
+  std::cout << "wrote message" << "\n";
 }
--- src/proc/gps/gps_main.cpp
+++ src/proc/gps/gps_main.cpp
@@ -3,6 +3,10 @@
 #include <log4cplus/configurator.h>
 #include <log4cplus/initializer.h>
 
+#include "cam_config_msg_DCPS.hpp"
+
+#include <iostream>
+
 int main()
 {
   log4cplus::Initializer init;
@@ -13,4 +17,29 @@ int main()
   log4cplus::Logger console_logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("Olaf_Console_Logger"));
 
   LOG4CPLUS_INFO(console_logger, LOG4CPLUS_TEXT("GPS main started"));
+
+  dds::domain::DomainParticipant participant(0);
+
+  dds::topic::Topic<Cam_Messages::Config_Message> topic(participant, "cam_messages_config_message");
+  dds::sub::Subscriber subscriber(participant);
+  dds::sub::DataReader<Cam_Messages::Config_Message> data_reader(subscriber, topic);
+
+  dds::core::cond::WaitSet wait_set;
+
+  dds::sub::cond::ReadCondition read_condition(data_reader, dds::sub::status::DataState::new_data());
+
+  wait_set += read_condition;
+
+  std::cout << "Subscriber waiting..." << "\n";
+
+  wait_set.wait();
+
+  auto samples = data_reader.read();
+
+  std::for_each(samples.begin(),
+                samples.end(),
+                [](const dds::sub::Sample<Cam_Messages::Config_Message> &message)
+                {
+                  std::cout << "Got it.\n";
+                });
 }
